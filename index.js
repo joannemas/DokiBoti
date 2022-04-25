75dc516c8ceeddedc8983774289aac1d859d4a89
Rayan - Laptop <rayan.idri@edu.devinci.fr>
2022-04-25 11:53:40 +0200

    Files Added

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] })
+// -------------------------------- //
+client.on('ready', function () {
+    console.log("Salut !")
+})
+// -------------------------------- //
+client.login('OTYwODQ1NTIyMTc0ODg1OTE4.YkwXIA.pKVuJldScngEyQxH0RFClZvfLnY')
