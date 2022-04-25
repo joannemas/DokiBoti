b62ba1f937efb63afad9e2ed62ac8736f6b2596f
Rayan - Laptop <rayan.idri@edu.devinci.fr>
2022-04-25 23:33:15 +0200

    Vocal creation system Added

--- vocal.js
+++ vocal.js
@@ -1,53 +1,51 @@
 const Discord = require('discord.js')
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"] })
+const client = new Discord.Client({
+    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]
+})
 // -------------------------------- //
 client.on('ready', function () {
     console.log("Vous êtes connecté au Bot.")
 })
 // -------------------------------- //
 
+let nameOfChannel = "";
+let limitUser= 0;
+let waitingForLimitUser = false;
+
 //Création d'un channel vocal
 client.on("messageCreate", message => {
     if (message.content === "vocal") {
+        const filter = (m) => m.author.id === message.author.id;
         message.channel.send("Comment souhaitez-vous appelez le salon ?")
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 1000 });
-            collector.on('collect', message => {
-                if (message.content === message.content ) {
-                    message.guild.channels.create(message.content, { 
-                        type: "GUILD_VOICE",
-                        permissionOverwrites: [{
-                        id: message.guild.id,
-                        allow: ['VIEW_CHANNEL'],
-                        }]
-                    });
-                } else if (message.content === "") {
-                    message.channel.send("Veuillez réessayer s'il vous plaît.");
-                }
-            })
-            return
-        message.channel.send("Channel vocal créé !");
-}})
-
-//Création d'un channel vocal avec sélection du nombre de User
-client.on("messageCreate", message => {
-    if (message.content === "!VocalLimité") {
-        message.channel.send("Combien de personnes peuvent accéder au vocal ? ");
-        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100 });
-            collector.on('collect', message => {
-                if (message.content >= 2 && message.content <= 99 ) {
-                    message.guild.channels.create("channel vocal", { 
-                        type: "GUILD_VOICE",
-                        userLimit: message.content,
-                        permissionOverwrites: [{
-                        id: message.guild.id,
-                        allow: ['VIEW_CHANNEL'],
-                        }]
-                    });
-                } else if (message.content < 2 || message.content > 99) {
-                    message.channel.send("Veuillez réessayer s'il vous plaît.");
-                }
-            })
-}})
+        const collector = new Discord.MessageCollector(message.channel, {
+            filter,
+            time: 20000
+        });
+        collector.on('collect', M => {
+            if (!waitingForLimitUser) {
+                nameOfChannel = M.content;
+                waitingForLimitUser = true;
+                message.channel.send("Combien d'user dans le vocal ? (0 pour infini)");
+                return;
+            }
+            if (isNaN(M.content)) {
+                message.channel.send("Veuillez tapez un nombre !");
+                return
+            }
+            limitUser = parseInt(M.content);
+            if (limitUser !== 0 && (limitUser <=1 || limitUser > 99)) {
+                message.channel.send("Veuillez réessayer, nombre incorrect ! Choisissez un nombre entre 2 & 99 !");
+                return
+            }
+            const options = {type: "GUILD_VOICE"}
+            if (limitUser !== 0) options.userLimit=limitUser;
+            message.guild.channels.create(nameOfChannel, options);
+            waitingForLimitUser = false;
+            message.channel.send(`Le channel ${nameOfChannel} à été crée avec succès !`);
+            collector.stop()
+        })
+    }
+})
 
 // -------------------------------- //
 client.login('OTYwODQ1NTIyMTc0ODg1OTE4.YkwXIA.pKVuJldScngEyQxH0RFClZvfLnY')
