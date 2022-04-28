b3daf1e88416ec6e1c7f0f486bdca69bf7502b42
Enas <enas.elmershati@gmail.com>
2022-04-28 19:52:29 +0200

    Add Commands

--- commands/start.js
+++ commands/start.js
@@ -4,11 +4,15 @@ module.exports = {
     
     execute(message, client) {
         if (message.content === "%start"){
-       
+            
+            message.guild.channels.create("Cours", { type: "GUILD_CATEGORY" });
             message.guild.channels.create("rendu");
+            message.guild.channels.create("bienvenue");
+            message.guild.channels.create("règles");
 
             channelCreated = true;
             client.channels.cache.find(channel => channel.name === "rendu").send("Vous verrez qui a envoyer son rendu et combien de personnes au total vous l'ont envoyer");
+            client.channels.cache.find(channel => channel.name === "règles").send("Règles");
         }
     }
 };
