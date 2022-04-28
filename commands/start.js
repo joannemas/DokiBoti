520fed669994feb4ba6dbeb4f8cc8c204257d397
Enas <enas.elmershati@gmail.com>
2022-04-28 20:41:36 +0200

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -2,13 +2,14 @@ module.exports = {
     name: 'start',
     description: 'Init the server',
     
-    execute(message, client) {
+    execute(message) {
         if (message.content === "%start"){
             
             message.guild.channels.create("Cours", { type: "GUILD_CATEGORY" });
+            message.guild.channels.create("Salons Vocaux", { type: "GUILD_CATEGORY" });
             message.guild.channels.create("rendu");
             message.guild.channels.create("bienvenue");
             message.guild.channels.create("règles");
-             }
+        }
     }
 };
