20b98fd0464e67d193d22a11a346339fbcefa214
Rayan - Laptop <rayan.idri@edu.devinci.fr>
2022-04-28 20:52:37 +0200

    ✨Add more features

--- embed.js
+++ embed.js
@@ -37,5 +37,34 @@ client.on("messageCreate", message => {
     }
 })
 
+client.on("messageCreate", message => {
+    let LogoBot = "http://pa1.narvii.com/6596/d9493dc295a4d548ba713e013d15ce3a90244446_00.gif";
+    if (message.content === "%commandes") {
+        const exampleEmbed = new MessageEmbed()
+            .setTitle("Liste des commandes du bot")
+            .setDescription("Voici les commandes pour utiliser les fonctionnalités du bot. \n ──────────────────────────────────────")
+            .setThumbnail(`${LogoBot}`)
+            .setColor("#FFFFFF")
+            .addFields(
+                { name: '%start', value: '➥ A lancé une fois lors de l\'installation du bot, créer les channels. \n ──────────────────────────────────────' },
+                { name: '%timer + durée+s|m|h', value: '➥ Lance un chronomètre et tag tout le monde à sa fin. \n ──────────────────────────────────────' },
+                { name: '%give + rôle + @nom', value: '➥ Ajouter un rôle à une personne. \n ──────────────────────────────────────' },
+                { name: '%remove+ rôle + @nom', value: '➥ Retir le rôle d\'une personne. \n ──────────────────────────────────────' },
+                { name: '%inspiration', value: '➥ Envoie des citations. \n ──────────────────────────────────────' },
+                { name: '%rendu', value: '➥ Dans le channel rendu, envoie qui et combien ont envoyé "%rendu". \n ──────────────────────────────────────' },
+                { name: '%stoprendu', value: '➥ Remet à 0 le compteur des rendu. \n ──────────────────────────────────────' },
+                { name: '%regles', value: '➥ Affiche la liste des règles du serveur. \n ──────────────────────────────────────' },
+                )
+            .setFooter({
+                text: 'Équipe en charge de l\'administration du serveur',
+                iconURL: `${LogoBot}`
+            })
+            .setTimestamp()
+        message.channel.send({
+            embeds: [exampleEmbed]
+        });
+    }
+})
+
 // -------------------------------- //
 client.login('OTYwODQ1NTIyMTc0ODg1OTE4.YkwXIA.pKVuJldScngEyQxH0RFClZvfLnY')
